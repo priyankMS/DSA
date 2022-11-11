b43e6756f1364682c06aab9d631010f9001a605a
priyank shiroya <97310194+priyankMS@users.noreply.github.com>
2022-11-11 23:16:40 +0530

    Add files via upload

---
+++ arraytwo.java
@@ -0,0 +1,15 @@
+class arraytwo{
+    public static void main(String args[])
+    {
+    int[] a[] = 
+    System.out.println(b[0]);
+    for (int i= 0; i< 3; i++)
+     {
+    for (int j = 0; j < 3; j++) {
+
+    a[i][j]="*";
+    System.out.println(a[i][j]);
+    }
+    }
+    }
+    }
